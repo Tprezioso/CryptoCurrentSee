9a7d9954211d5b18abd1385c92ccc28e7cb880c4
Tprezioso <tommyprezioso@gmail.com>
2018-07-23 15:53:50 -0400

    first comment

---
+++ CryptoCurrentSee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoCurrentSee
+//
+//  Created by Thomas Prezioso on 7/23/18.
+//  Copyright © 2018 Thomas Prezioso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
