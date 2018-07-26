73ff312c22522d30f8c80043c394986cd93397e6
Tprezioso <tommyprezioso@gmail.com>
2018-07-25 20:52:24 -0400

    added api call file

---
+++ CryptoCurrentSee/APICall.swift
@@ -0,0 +1,26 @@
+//
+//  APICall.swift
+//  
+//
+//  Created by Thomas Prezioso on 7/25/18.
+//
+
+import Cocoa
+import Alamofire
+
+class APICall: NSObject {
+    class func getCurrency(completion: @escaping ([String]) -> void){
+//        Alamofire.request("https://status.github.com/api/status.json").responseJSON { response in
+//            self.view.addSubview(hud)
+//            if let JSON = response.result.value {
+//                let data = JSON as? [String: Any]
+//                let status = data?["status"] as! String?
+//                self.statusLabel.text = " Status: \(status!.capitalizingFirstLetter())"
+//                let date = data?["last_updated"] as! String?
+//                self.setBackGroundColorForStatus(status: status!)
+//                self.lastUpdatedLabel.text = self.getDateFromJSONDate(dateString: date!)
+//                hud.hide(afterDelay: 0.5)
+//            }
+//        }
+    }
+}
