e3f7534c025607d85528670a433ee6d974cfefd8
Tprezioso <tommyprezioso@gmail.com>
2018-08-16 18:24:55 -0400

    working....

--- CryptoCurrentSee/ViewController.swift
+++ CryptoCurrentSee/ViewController.swift
@@ -12,16 +12,11 @@ class ViewController: UIViewController {
 
     var jsonData = ""
     var aSTRINTG = ""
-    
-        
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let api = APICall.self
         api.getCurrency(completion: jsonData)
-        //let json = api.listOfCryptoCurrency(completion: jsonData)
-        print(aSTRINTG)
-    
     }
 }
 
